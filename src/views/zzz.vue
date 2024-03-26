9c71505775ee94302839a0087ac04bfe5d317470
finn-rm <finn.rawles@canonical.com>
2024-03-26 17:52:53 -0300

    Changed wording

--- src/views/zzz.vue
+++ src/views/zzz.vue
@@ -3,7 +3,7 @@
     <v-container fill-height>
         <v-row justify="center" align="center" no-gutters class="grey">
           <v-col cols="7" align="center" xs12 sm8 md8 lg8 class="grey lighten-5 pa-10">
-            <div class="mb-7 text-h2 text-center">The Canonical help desk is currently closed, try again later.</div>
+            <div class="mb-7 text-h2 text-center">The event help desk is currently closed, try again later.</div>
             <v-img style="max-height: 300px" src="/sleeping.png"></v-img>
             <v-form ref="form">
               <div height="100px" class="mt-0" dense></div>
